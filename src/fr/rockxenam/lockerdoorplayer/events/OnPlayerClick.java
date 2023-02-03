5b5734b3b64166791a1f0a77255d888d05d2d1f3
LeGinch <40724705+LeGinch@users.noreply.github.com>
2023-02-03 22:28:07 +0100

    I modified OnPlayerClick.java class !
    the former version has a bug, 
    the player is disconnected every 5 minutes in game.
    
    i modified the file and the conditions.
    
    this new version doesn't bug anymore !

--- src/fr/rockxenam/lockerdoorplayer/events/OnPlayerClick.java
+++ src/fr/rockxenam/lockerdoorplayer/events/OnPlayerClick.java
@@ -26,76 +26,9 @@ public class OnPlayerClick implements Listener {
         Player p = e.getPlayer();
         Action a = e.getAction();
 
-        if (p.getInventory().getItemInMainHand().getType().equals(Material.STONE_SWORD) && p.isOp()) {
-            if (a.equals(Action.RIGHT_CLICK_BLOCK)) {
-                if (p.getTargetBlock(null, 100).getType().toString().contains("DOOR")) {
-                    Location targetLocation = p.getTargetBlock(null, 100).getLocation();
-                    double targetX = targetLocation.getX();
-                    double targetY = targetLocation.getY();
-                    double targetZ = targetLocation.getZ();
-                    double verifyUpY = targetY + 1;
-                    double goodY;
-                    Location doorUpLocation = new Location(e.getPlayer().getWorld(), targetX, verifyUpY, targetZ);
 
-                    if (doorUpLocation.getBlock().getType().toString().contains("DOOR")) {
-                        goodY = targetY;
-                    } else {
-                        goodY = targetY - 1;
-                    }
-                    String n = "" + String.valueOf(targetX) + "" + String.valueOf(goodY) + "" + String.valueOf(targetZ);
-                    String name = n.replaceAll("\\p{Punct}", "");
-
-                    double dYF = instance.getDataLoader().getYByName(name);
-
-                    if (dYF != 0) { //si la position existe
-                        this.messages.getDoorExist(p, String.valueOf(name));
-                    } else {
-                        instance.getDataLoader().addDoor(name, targetX, goodY, targetZ);
-                        this.messages.getSetDoor(p, name);
-
-                    }
-                }
-            }
-        } else if (p.getInventory().getItemInMainHand().getType().equals(Material.STONE_SWORD) && !p.isOp()) {
-            if (a.equals(Action.RIGHT_CLICK_BLOCK)) {
-                if (p.getTargetBlock(null, 100).getType().toString().contains("DOOR")) {
-                    Location targetLocation = p.getTargetBlock(null, 100).getLocation();
-                    double targetX = targetLocation.getX();
-                    double targetY = targetLocation.getY();
-                    double targetZ = targetLocation.getZ();
-                    double verifyUpY = targetY + 1;
-                    double goodY;
-                    Location doorUpLocation = new Location(e.getPlayer().getWorld(), targetX, verifyUpY, targetZ);
-
-                    if (doorUpLocation.getBlock().getType().toString().contains("DOOR")) {
-                        goodY = targetY;
-                    } else {
-                        goodY = targetY - 1;
-                    }
-                    String n = "" + String.valueOf(targetX) + "" + String.valueOf(goodY) + "" + String.valueOf(targetZ);
-                    String name = n.replaceAll("\\p{Punct}", "");
-
-                    double dYF = instance.getDataLoader().getYByName(name);
-
-                    if (dYF != 0) { //si la position existe
-                        String owner = instance.getDataLoader().getOwnerbyName(name);
-                        if (owner.equals("06_78//45-jrp87458@@fjfdffhfg145784d")) {
-
-                        } else {
-                            if (owner.equals(p.getName())) {
-
-                            } else {
-                                e.setCancelled(true);
-                                this.messages.getNoPermToUse(p, name, owner);
-
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        if (a.equals(Action.RIGHT_CLICK_BLOCK) && !p.getInventory().getItemInMainHand().getType().equals(Material.STONE_SWORD) && !p.isOp()) {
-            if (p.getTargetBlock(null, 100).getType().toString().contains("DOOR")) {
+        if(a.equals(Action.RIGHT_CLICK_BLOCK) && p.getTargetBlock(null, 100).getType().toString().contains("DOOR")){
+            if(p.isOp() && p.getInventory().getItemInMainHand().getType().equals(Material.STONE_SWORD)){
                 Location targetLocation = p.getTargetBlock(null, 100).getLocation();
                 double targetX = targetLocation.getX();
                 double targetY = targetLocation.getY();
@@ -106,10 +39,34 @@ public class OnPlayerClick implements Listener {
 
                 if (doorUpLocation.getBlock().getType().toString().contains("DOOR")) {
                     goodY = targetY;
-                    //p.sendMessage("vous avez cliqué sur le bas de la porte");
                 } else {
                     goodY = targetY - 1;
-                    //.sendMessage("vous avez cliqué sur le haut de la porte");
+                }
+                String n = "" + String.valueOf(targetX) + "" + String.valueOf(goodY) + "" + String.valueOf(targetZ);
+                String name = n.replaceAll("\\p{Punct}", "");
+
+                double dYF = instance.getDataLoader().getYByName(name);
+
+                if (dYF != 0) { //si la position existe
+                    this.messages.getDoorExist(p, String.valueOf(name));
+                } else {
+                    instance.getDataLoader().addDoor(name, targetX, goodY, targetZ);
+                    this.messages.getSetDoor(p, name);
+
+                }
+            } else {
+                Location targetLocation = p.getTargetBlock(null, 100).getLocation();
+                double targetX = targetLocation.getX();
+                double targetY = targetLocation.getY();
+                double targetZ = targetLocation.getZ();
+                double verifyUpY = targetY + 1;
+                double goodY;
+                Location doorUpLocation = new Location(e.getPlayer().getWorld(), targetX, verifyUpY, targetZ);
+
+                if (doorUpLocation.getBlock().getType().toString().contains("DOOR")) {
+                    goodY = targetY;
+                } else {
+                    goodY = targetY - 1;
                 }
                 String n = "" + String.valueOf(targetX) + "" + String.valueOf(goodY) + "" + String.valueOf(targetZ);
                 String name = n.replaceAll("\\p{Punct}", "");
@@ -131,33 +88,10 @@ public class OnPlayerClick implements Listener {
             }
         }
 
-        if (a.equals(Action.LEFT_CLICK_BLOCK) && !p.isOp()) {
-            if (p.getTargetBlock(null, 100).getType().toString().contains("DOOR")) {
-                Location targetLocation = p.getTargetBlock(null, 100).getLocation();
-                double targetX = targetLocation.getX();
-                double targetY = targetLocation.getY();
-                double targetZ = targetLocation.getZ();
-                double verifyUpY = targetY + 1;
-                double goodY;
-                Location doorUpLocation = new Location(e.getPlayer().getWorld(), targetX, verifyUpY, targetZ);
 
-                if (doorUpLocation.getBlock().getType().toString().contains("DOOR")) {
-                    goodY = targetY;
-                } else {
-                    goodY = targetY - 1;
-                }
-                String n = "" + String.valueOf(targetX) + "" + String.valueOf(goodY) + "" + String.valueOf(targetZ);
-                String name = n.replaceAll("\\p{Punct}", "");
 
-                double dYF = instance.getDataLoader().getYByName(name);
-
-                if (dYF != 0) { //si la position existe
-                    this.messages.getNoPermToBreak(p, name);
-                    e.setCancelled(true);
-                }
-            }
-        } else if (p.isOp() && a.equals(Action.LEFT_CLICK_BLOCK)) {
-            if (p.getTargetBlock(null, 100).getType().toString().contains("DOOR")) {
+        if(a.equals(Action.LEFT_CLICK_BLOCK) && p.getTargetBlock(null, 100).getType().toString().contains("DOOR")){
+            if(p.isOp()){
                 Location targetLocation = p.getTargetBlock(null, 100).getLocation();
                 double targetX = targetLocation.getX();
                 double targetY = targetLocation.getY();
@@ -179,6 +113,29 @@ public class OnPlayerClick implements Listener {
                 if (dYF != 0) { //si la position existe
                     this.messages.getRemoveDoor(p, name);
                     instance.getDataLoader().removeDoor(name);
+                }
+            } else {
+                Location targetLocation = p.getTargetBlock(null, 100).getLocation();
+                double targetX = targetLocation.getX();
+                double targetY = targetLocation.getY();
+                double targetZ = targetLocation.getZ();
+                double verifyUpY = targetY + 1;
+                double goodY;
+                Location doorUpLocation = new Location(e.getPlayer().getWorld(), targetX, verifyUpY, targetZ);
+
+                if (doorUpLocation.getBlock().getType().toString().contains("DOOR")) {
+                    goodY = targetY;
+                } else {
+                    goodY = targetY - 1;
+                }
+                String n = "" + String.valueOf(targetX) + "" + String.valueOf(goodY) + "" + String.valueOf(targetZ);
+                String name = n.replaceAll("\\p{Punct}", "");
+
+                double dYF = instance.getDataLoader().getYByName(name);
+
+                if (dYF != 0) { //si la position existe
+                    this.messages.getNoPermToBreak(p, name);
+                    e.setCancelled(true);
                 }
             }
         }
